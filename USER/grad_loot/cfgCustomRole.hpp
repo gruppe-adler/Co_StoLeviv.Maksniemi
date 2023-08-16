6957b0c556e55cb79c3e25d7cbee857efe46c6d9
simon schmidt <nomisum@googlemail.com>
2023-08-16 20:15:56 +0200

    cfgcustomrole and cfgloot template

---
+++ USER/grad_loot/cfgCustomRole.hpp
@@ -0,0 +1,11 @@
+class repairVehicles {
+    role = "Engineer";
+    briefing = "You can repair any vehicle with your golden hands.";
+    code = "(_this select 0) setVariable ['ACE_isEngineer', 2, true];";
+};
+
+class convoy {
+   role = "convoyintel";
+   briefing = "My mother told me every half hour there is a convoy packed with ammunition running down the central highway from west to east.";
+   code = "";
+};
---
+++ USER/grad_loot/cfgLoot.hpp
@@ -0,0 +1,8 @@
+class flinte {
+    classname = "rhs_weap_m38";
+    markerPrefix = "mrk_loot_flinte";
+    buried = 0;
+    briefing = "My grandpa was a hunter and he might have useful stuff in his cabin in the woods";
+    picture = "";
+    code = "";
+};
