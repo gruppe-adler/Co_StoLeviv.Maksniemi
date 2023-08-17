7ced2203eac8807293546af26487d2efc9b409ab
simon schmidt <nomisum@googlemail.com>
2023-08-17 10:42:52 +0200

    add customintel

---
+++ USER/grad_loot/cfgCustomIntel.hpp
@@ -0,0 +1,5 @@
+class convoy {
+   role = "convoyintel";
+   briefing = "My mother told me every half hour there is a convoy packed with ammunition running down the central highway from west to east.";
+   code = "";
+};
--- USER/grad_loot/cfgCustomRole.hpp
+++ USER/grad_loot/cfgCustomRole.hpp
@@ -1,11 +1,18 @@
-class repairVehicles {
+class engineer {
     role = "Engineer";
     briefing = "You can repair any vehicle with your golden hands.";
     code = "(_this select 0) setVariable ['ACE_isEngineer', 2, true];";
 };
 
-class convoy {
-   role = "convoyintel";
-   briefing = "My mother told me every half hour there is a convoy packed with ammunition running down the central highway from west to east.";
-   code = "";
+class necromancer {
+    role = "Necromancer";
+    briefing = "You are a weirdo. Why? You are convinced you are a medium an you can speak to the dead.";
+    code = "(_this select 0) call grad_necromancing_fnc_initNecro;";
 };
+
+class emergency {
+    role = "Emergency Doctor";
+    briefing = "You are a neutral party and cannot carry weapons. <br/><br/>The most important thing is: Russians won't open fire on you.";
+    code = "(_this select 0) call grad_customMedicSystem_fnc_initMedic;";
+};
+
