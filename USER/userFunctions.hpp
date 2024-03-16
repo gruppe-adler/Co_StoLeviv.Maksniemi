887ae00709f33e10f36ab83f40c00a7df6b0983f
nomisum <nomisum@gmail.com>
2024-03-16 15:26:13 +0100

    sdfsdf

--- USER/userFunctions.hpp
+++ USER/userFunctions.hpp
@@ -3,12 +3,14 @@
 *   Ist in CfgFunctions included.
 */
 
-#include "grad_grasscutter\cfgFunctions.hpp"
-#include "grad_customMedicSystem\cfgFunctions.hpp"
-#include "grad_loot\cfgFunctions.hpp"
+#include "grad_briefing\cfgFunctions.hpp"
 #include "grad_buoys\cfgFunctions.hpp"
-#include "grad_fish\cfgFunctions.hpp"
 #include "grad_clothing\cfgFunctions.hpp"
-#include "grad_tasks\cfgFunctions.hpp"
+#include "grad_customMedicSystem\cfgFunctions.hpp"
+#include "grad_fish\cfgFunctions.hpp"
+#include "grad_grasscutter\cfgFunctions.hpp"
 #include "grad_roles\cfgFunctions.hpp"
+#include "grad_loot\cfgFunctions.hpp"
+#include "grad_tasks\cfgFunctions.hpp"
+#include "grad_zeus\cfgFunctions.hpp"
 #include "..\node_modules\grad-leaveNotes\cfgFunctions.hpp"
