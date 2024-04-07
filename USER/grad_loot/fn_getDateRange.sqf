80b5d11f1ff40b1cdc3087446d944da713595445
JulesVerner <julius.lenz@outlook.com>
2024-04-07 12:45:16 +0200

    adjusted tombstone epitaphs and date ranges

--- USER/grad_loot/fn_getDateRange.sqf
+++ USER/grad_loot/fn_getDateRange.sqf
@@ -8,12 +8,12 @@ while { count _dates < 900 } do {
 
 	private _min = 1880;
 	private _mid = 1950;
-	private _max = 2005;
+	private _max = 2010;
 	private _yearbirth = round (random [_min, _mid, _max]);
 
 	private _daydeath = [1,30] call BIS_fnc_randomInt;
 	private _monthdeath = [1,12] call BIS_fnc_randomInt;
-	private _yeardeath = round (random [_yearbirth, _yearbirth + 45, _yearbirth + 90]) min 2005;
+	private _yeardeath = round (random [_yearbirth, _yearbirth + 45, _yearbirth + 90]) min 2010;
 
 	// we dont want to calculate real dates really
 	if (_yearbirth == _yeardeath) then {
--- USER/missionInfo.hpp
+++ USER/missionInfo.hpp
@@ -4,7 +4,7 @@
 
 author = "nomisum für Gruppe Adler";                                            // Missionsersteller
 onLoadName = "Co Sto Leviv";                                                    // Name der Mission
-onLoadMission = "Each player starts on his own.";                               // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
+onLoadMission = "Each player starts on their own.";                               // Beschreibung der Mission (wird im Ladebildschirm unterhalb des Ladebildes angezeigt)
 loadScreen = "data\loading.jpg";                                                // Ladebild
 overviewPicture = "";                                                           // Bild, das in der Missionsauswahl angezeigt wird
 overviewText = "";                                                              // Text, der in der Missionsauswahl angezeigt wird
